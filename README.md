00276b936df8f00830ca64f70a0ef3edc13e2b34
smellycat5 <prajapatichunan@gmail.com>
2023-07-08 23:09:34 +0545

    updated readme

--- README.md
+++ README.md
@@ -1,5 +1,18 @@
 <p align="center"><a href="https://laravel.com" target="_blank"><img src="https://raw.githubusercontent.com/laravel/art/master/logo-lockup/5%20SVG/2%20CMYK/1%20Full%20Color/laravel-logolockup-cmyk-red.svg" width="400" alt="Laravel Logo"></a></p>
 
+## About This Project
+Job Seeker is a Job finding managing web application where you can post jobs to find the best suited candidate for your organization. The Web version of this project is still a in progress. But the API is ready to use, following the below documentation.
+
+Absolutely any feedback on the project is welcomed. 
+
+Reach me at https://www.linkedin.com/in/chunan/
+
+## How To Install:
+1. Download the ZIP file or clone the repository.
+2. Install dependencies by running "composer install" in your terminal.
+3. Set up your database in the .env file.
+4. Run Database Migrations using "php artisan migrate".
+5. Use the "php artisan serve" command to run it on your local server.{ http://127.0.0.1:8000 on your browser(may differ according to your configs)}
 ## API Documentaion for the project
 
 https://documenter.getpostman.com/view/26943794/2s93zB72wc
--- resources/views/components/createOrganization.blade.php
+++ resources/views/components/createOrganization.blade.php
@@ -1,33 +1,31 @@
 @extends('layouts.base')
 @section('content')
-<div class="content-container">
-    <section>
-        <div class="container">
-            <form class="login-form" method="POST" action="{{ route('organization.store') }}">
-                <h3 class="center-text">Enter Organization Details</h3>
-                
-                @csrf
-                
-                <div>
-                    
-                    <input class="mt-4" type="text" id="name" name="name" placeholder="Name" value="{{ old('name') }}" required
-                    autofocus>
-                </div>
-                
-                <div>
-                    <input class="mt-4"type="text" id="location" name="location" placeholder="Location" value="{{ old('location') }}"
-                    required>
-                </div>
-                
-                <div>
-                    <input class="mt-4" type="email" id="email" name="email" placeholder="Email" value="{{ old('email') }}" required>
-                </div>
-                <input type="hidden" name="user_id" value={{ auth()->user()->id}}>
-                <div>
-                    <button type="submit">Submit</button>
-                </div>
-            </form>
-        </div>
-    </section>
-</div>
+    <div class="content-container">
+        <section>
+            <div class="container">
+                <form class="login-form" method="POST" action="{{ route('organization.store') }}">
+                    <h3 class="center-text">Enter Organization Details</h3>
+                    @csrf
+                    <div>
+                        <input class="mt-4" type="text" id="name" name="name" placeholder="Name"
+                            value="{{ old('name') }}" required autofocus>
+                    </div>
+
+                    <div>
+                        <input class="mt-4"type="text" id="location" name="location" placeholder="Location"
+                            value="{{ old('location') }}" required>
+                    </div>
+
+                    <div>
+                        <input class="mt-4" type="email" id="email" name="email" placeholder="Email"
+                            value="{{ old('email') }}" required>
+                    </div>
+                    <input type="hidden" name="user_id" value={{ auth()->user()->id }}>
+                    <div>
+                        <button type="submit">Submit</button>
+                    </div>
+                </form>
+            </div>
+        </section>
+    </div>
 @endsection
